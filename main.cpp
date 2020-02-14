78ad993a669254d6d895f553a84a0d7b985affb0
https://AlValBel@bitbucket.org <alexisvabel@gmail.com>
2020-02-14 17:00:40 +0700

    Added nuklear lib, need to process it. Not sure to use it in real proj

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+
+#include "lib/nuklear.h"
+#include "lib/nuklear_xlib.h"
+
+
 #include <getopt.h>
 #include <memory>
+#include <cstdarg>
+#include <sys/time.h>
 #include "vsqconstructor.h"
 
 static const char *optString = "p:";
@@ -28,6 +35,54 @@ void read_cmd_conf(int &argc, char ** argv){
         fill_cmd_args(opt);
     }while (opt != -1);
 }
+
+
+typedef struct XWindow XWindow;
+struct XWindow {
+    Display *dpy;
+    Window root;
+    Visual *vis;
+    Colormap cmap;
+    XWindowAttributes attr;
+    XSetWindowAttributes swa;
+    Window win;
+    int screen;
+    XFont *font;
+    unsigned int width;
+    unsigned int height;
+    Atom wm_delete_window;
+};
+
+static void
+die(const char *fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    va_end(ap);
+    fputs("\n", stderr);
+    exit(EXIT_FAILURE);
+}
+
+static long
+timestamp(void)
+{
+    struct timeval tv;
+    if (gettimeofday(&tv, NULL) < 0) return 0;
+    return (long)((long)tv.tv_sec * 1000 + (long)tv.tv_usec/1000);
+}
+
+static void
+sleep_for(long t)
+{
+    struct timespec req;
+    const time_t sec = (int)(t/1000);
+    const long ms = t - (sec * 1000);
+    req.tv_sec = sec;
+    req.tv_nsec = ms * 1000000L;
+    while(-1 == nanosleep(&req, &req));
+}
+
 
 
 
